dbbf3ea08c6bb6dbb2dacd9b2e576853ceca6652
Supriyo <supriyokanrar626@gmail.com>
2026-07-15 09:17:44 +0530

    Time: 0 ms (100%), Space: 8.5 MB (85.02%) - LeetHub

--- 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.cpp
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     int gcdOfOddEvenSums(int n) {
-        int sumOdd = n * n ; 
-        int sumEven = n * (n+1) ;
+        int sumOdd = n * n ; // formula -> first n ta odd number ar sum..sum = n * n ..
+        int sumEven = n * (n+1) ;  // same.. 
 
         return gcd(sumOdd, sumEven) ;
     }
