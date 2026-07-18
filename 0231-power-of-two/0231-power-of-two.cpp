0d6b0d293f9a30ca284d664ad7f56d7ef9139ed7
Supriyo <supriyokanrar626@gmail.com>
2026-07-18 21:36:39 +0530

    Time: 0 ms (100%), Space: 7.9 MB (50.21%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -1,20 +1,27 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
+
+        // using bit manipulation...Below ..
+        // if(n > 0 && (n & (n-1)) == 0) {
+        //     return true ;
+        // } else {
+        //     return false ;
+        // }
         
-        // if(n <= 0)
+         // if(n <= 0)
         // {
         //     return false;
         // } else{
         //     return log2(n) == floor(log2(n)) ;
         // }
-
-        // using bit manipulation...Below ..
-        if(n > 0 && (n & (n-1)) == 0) {
-            return true ;
-        } else {
-            return false ;
-        }
         
+        // using recursion..
+
+        if(n <= 0) return false ;  // -ve ar jonno 
+        if(n == 1) return true ;
+        if(n % 2 != 0) return false ; // n odd hole
+
+        return isPowerOfTwo(n/2) ; // n ke 2 dia vag kore abar check..
     }
 };
