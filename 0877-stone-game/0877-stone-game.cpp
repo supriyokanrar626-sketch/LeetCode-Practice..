1b322666c3ca5560eb251410a178aaa761497940
Supriyo <supriyokanrar626@gmail.com>
2026-08-02 15:02:53 +0530

    Time: 0 ms (100%), Space: 10.5 MB (68.24%) - LeetHub

---
+++ 0877-stone-game/0877-stone-game.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    bool stoneGame(vector<int>& piles) {
+        // sum(even indexed piles) != sum(odd indexed piles)
+        // Therefore, one of these two groups must have a larger sum.
+        // alice can choose any group which is sure to win..
+         return true ;
+    }
+};
